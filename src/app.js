c9b5f5373f38f273bbdfb6bce5305825dd804a22
Kelsch <dalankelsch@hotmail.com>
2019-07-19 10:27:37 -0600

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// To start this app type: npm run start
+// In the terminal
+
+import { MDCTopAppBar } from '@material/top-app-bar';
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+import {MDCDrawer} from "@material/drawer";
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+const modalButton = document.querySelector('.mdc-ripple-upgraded--unbounded');
+
+modalButton.addEventListener('click', (event) => {
+    console.log(event)
+    drawer.open = true;
+});
---
+++ src/lib.js
@@ -0,0 +1,8 @@
+export const person = {
+    name: 'John Doe',
+    age: 35
+}
+
+export function sayHello(name) {
+    return `Hello ${name}`;
+}
